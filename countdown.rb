1cecb018d4df5a8ef9a352bdd7ab33595b063065
Laura Hoffman <lhoffmanwg@gmail.com>
2019-01-31 03:42:57 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  counter = 0
+  while counter < number
+    backward == number -= 1 
+    puts "#{backward} SECOND(S)!"
+  counter = += 1 
+end
 end
